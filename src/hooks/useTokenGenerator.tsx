19435be2c3565a661d38f981d6772083bd5c20c2
Neil Dwyer <neildwyer1991@gmail.com>
2024-03-12 15:41:38 -0700

    token generator context

---
+++ src/hooks/useTokenGenerator.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from "react";
+
+type TokenGeneratorData = {
+  generateConnectionDetails: () => Promise<void>
+  connectionDetails: {token: string, url: string} | undefined
+}
+
+const TokenGeneratorContext = React.createContext<TokenGeneratorData | undefined>(undefined);
+
+type Props = {
+  tokenGenerator: () => Promise<{ token: string; url: string }>;
+  children: React.ReactNode;
+};
+
+function TokenGeneratorProvider({tokenGenerator, children}: Props) {
+  const [connectionDetails, setConnectionDetails] = React.useState<
+    { token: string; url: string } | undefined
+  >(undefined);
+
+  const generateConnectionDetails = useCallback(async () => {
+    const details = await tokenGenerator();
+    setConnectionDetails(details);
+  }, [tokenGenerator])
+
+  return (
+    <TokenGeneratorContext.Provider value={{ generateConnectionDetails, connectionDetails }}>
+      {children}
+    </TokenGeneratorContext.Provider>
+  );
+}
+
+function useTokenGenerator() {
+  const context = React.useContext(TokenGeneratorContext);
+  if (!context) {
+    throw new Error("useTokenGenerator must be used within a TokenGeneratorProvider");
+  }
+  return context;
+}
