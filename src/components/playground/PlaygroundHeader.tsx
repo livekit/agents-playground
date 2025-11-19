612b947ca0d697bc0b778359bd98dac155916922
Eugene Cross <eugene.cross@gmail.com>
2025-11-19 09:20:08 +0000

    fix: remove livekit link from header icon

--- src/components/playground/PlaygroundHeader.tsx
+++ src/components/playground/PlaygroundHeader.tsx
@@ -45,7 +45,7 @@ export const PlaygroundHeader = ({
     >
       <div className="flex items-center gap-3 basis-2/3">
         <div className="flex lg:basis-1/2">
-          <a href="https://livekit.io">{logo ?? <LKLogo />}</a>
+          <a href="#">{logo ?? <LKLogo />}</a>
         </div>
         <div className="lg:basis-1/2 lg:text-center text-xs lg:text-base lg:font-semibold text-skin-primary">
           {title}
